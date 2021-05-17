4d0d35082a1cbe7fdc7f3e65477a0d24a84ba593
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-17 19:13:56 -0400

    Update Message.ts

--- src/Events/Message/Message.ts
+++ src/Events/Message/Message.ts
@@ -22,67 +22,69 @@ export default class MessageEvent extends BaseEvent {
 				.split(/\s+/);
 			const command = client.commands.get(cmdName);
 
-			if (
-				command.getOwneronly() == true &&
-				message.author.id != '398264990567628812'
-			) {
-				const embed = await this.ErrorEmbed.Base({
-					iconURL: message.author.displayAvatarURL({ dynamic: true }),
-					text: 'Message event',
-					id: message.guild.id,
-					error_message: 'This command is owner only!',
-				});
-
-				const msg = await message.channel.send({ embed: embed });
-				return msg.delete({ timeout: 10000 });
-			}
-
-			if (command.getNsfw() == true && message.channel.nsfw == false) {
-				const embed = await this.ErrorEmbed.NsfwError({
-					iconURL: message.author.displayAvatarURL({ dynamic: true }),
-					text: 'Message event',
-					id: message.guild.id,
-				});
-
-				const msg = await message.channel.send({ embed: embed });
-				return msg.delete({ timeout: 10000 });
-			}
-
-			if (
-				command.getStatus() == 'WIP' ||
-				(command.getStatus() == 'debug' &&
-					message.author.id != '398264990567628812')
-			) {
-				const embed = await this.ErrorEmbed.Base({
-					iconURL: message.author.displayAvatarURL({ dynamic: true }),
-					text: 'Message event',
-					id: message.guild.id,
-					error_message: 'This command is currently off limits',
-				});
-
-				const msg = await message.channel.send({ embed: embed });
-				return msg.delete({ timeout: 10000 });
-			}
-
 			if (command) {
+				if (
+					command.getOwneronly() == true &&
+					message.author.id != '398264990567628812'
+				) {
+					const embed = await this.ErrorEmbed.Base({
+						iconURL: message.author.displayAvatarURL({ dynamic: true }),
+						text: 'Message event',
+						id: message.guild.id,
+						error_message: 'This command is owner only!',
+					});
+
+					const msg = await message.channel.send({ embed: embed });
+					return msg.delete({ timeout: 10000 });
+				}
+
+				if (command.getNsfw() == true && message.channel.nsfw == false) {
+					const embed = await this.ErrorEmbed.NsfwError({
+						iconURL: message.author.displayAvatarURL({ dynamic: true }),
+						text: 'Message event',
+						id: message.guild.id,
+					});
+
+					const msg = await message.channel.send({ embed: embed });
+					return msg.delete({ timeout: 10000 });
+				}
+
+				if (
+					command.getStatus() == 'WIP' ||
+					(command.getStatus() == 'debug' &&
+						message.author.id != '398264990567628812')
+				) {
+					const embed = await this.ErrorEmbed.Base({
+						iconURL: message.author.displayAvatarURL({ dynamic: true }),
+						text: 'Message event',
+						id: message.guild.id,
+						error_message: 'This command is currently off limits',
+					});
+
+					const msg = await message.channel.send({ embed: embed });
+					return msg.delete({ timeout: 10000 });
+				}
+
 				command.run(client, message, cmdArgs);
 
 				const res = await con.query(`SELECT commandsused FROM botstats`);
 				const response = await con.query(
 					`SELECT commandsused FROM guildstats WHERE guildId = '${message.guild.id}'`
 				);
-				let total: number = parseInt(res.rows[0].commandsused);
-				total++;
+				console.log(res.rows);
+				console.log(response.rows);
+				//		let total: number = parseInt(res.rows[0].commandsused);
+				//				total++;
 
-				let guildTotal: number = parseInt(response.rows[0].commandsused);
-				guildTotal++;
+				//	let guildTotal: number = parseInt(response.rows[0].commandsused);
+				//				guildTotal++;
 				try {
-					await con.query(`BEGIN`);
-					await con.query(`UPDATE botstats SET commandsused = '${total}'`);
-					await con.query(
-						`UPDATE guildstats SET commandsused = '${guildTotal}'`
-					);
-					await con.query(`COMMIT`);
+					//				await con.query(`BEGIN`);
+					//				await con.query(`UPDATE botstats SET commandsused = '${total}'`);
+					//				await con.query(
+					//					`UPDATE guildstats SET commandsused = '${guildTotal}'`
+					//				);
+					//				await con.query(`COMMIT`);
 				} catch (error) {
 					console.log(error);
 				} finally {
