b35f5bd4472d2b907471e8f1d9160f5a9ea9c1ca
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-27 20:36:30 -0400

    error fix

--- src/Events/Guilds/GuildCreate.ts
+++ src/Events/Guilds/GuildCreate.ts
@@ -12,7 +12,7 @@ export default class GuildCreateEvent extends BaseEvent {
 		try {
 			await con.query(`BEGIN`);
 			await con.query(
-				`INSERT INTO Guilds(guildid, welcome, leave, roles, logging, blacklisted, disableditems, moderations, protected, ranks, tags) VALUES('${
+				`INSERT INTO Guilds(guildid, welcome, leave, roles, logging, blacklisted, disableditems, moderations, protected, ranks, tags, notes) VALUES('${
 					guild.id
 				}', '${new Schemas.Welcome().toString()}', '${new Schemas.Leave().toString()}', '${new Schemas.Roles().toString()}', '${new Schemas.Logging().toString()}', '${new Schemas.Blacklisted().toString()}', '${new Schemas.Disabled().toString()}', '${new Schemas.Moderations().toString()}', '${new Schemas.Protected().toString()}', '${new Schemas.Ranks().toString()}', '${new Schemas.Tags().toString()}', '${new Schemas.Notes().toString()}')`
 			);
