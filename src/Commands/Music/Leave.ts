feefcd6ed42ded57a568de8b4ec1eba1b007648b
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-10-01 09:56:19 -0400

    Disabled because of YouTube ToS

--- src/Commands/Music/Leave.ts
+++ src/Commands/Music/Leave.ts
@@ -19,7 +19,7 @@ export default class LeaveCommand extends BaseCommand {
 			false,
 			false,
 			3000,
-			'working'
+			'WIP'
 		);
 	}
 	async run(client: DiscordClient, message: Message, args: string[]) {
