b247a08948e7bb31e2ea1712e42c5f82c54c93f7
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-10-26 10:19:40 -0400

    yes

--- src/Commands/Server Utilities/Avatar.ts
+++ src/Commands/Server Utilities/Avatar.ts
@@ -1,90 +1,90 @@
-import BaseCommand from '../../Utils/Structures/BaseCommand';
-import DiscordClient from '../../Client/Client';
-import { CommandInteraction, Message } from 'discord.js';
+import BaseCommand from "../../Utils/Structures/BaseCommand";
+import DiscordClient from "../../Client/Client";
+import { CommandInteraction, Message } from "discord.js";
 
 export default class AvatarCommand extends BaseCommand {
-	constructor() {
-		super(
-			'avatar',
-			'server utilities',
-			[],
-			'(mention)',
-			'Returns the mentioned users avatar or your avatar',
-			'',
-			[],
-			[],
-			['SEND_MESSAGES', 'EMBED_LINKS'],
-			[],
-			true,
-			false,
-			false,
-			3000,
-			'debug'
-		);
-	}
-	async run(client: DiscordClient, message: Message, args: string[]) {
-		const user =
-			message.mentions.members.first() ||
-			message.guild.members.cache.find((u) => u.user.id == args[0]) ||
-			message.guild.members.cache.find((u) => u.user.username == args[0]) ||
-			message.guild.members.cache.find((u) => u.nickname == args[0]);
-		const argument = args[0];
+  constructor() {
+    super(
+      "avatar",
+      "server utilities",
+      [],
+      "(mention)",
+      "Returns the mentioned users avatar or your avatar",
+      "",
+      [],
+      [],
+      ["SEND_MESSAGES", "EMBED_LINKS"],
+      [],
+      true,
+      false,
+      false,
+      3000,
+      "working"
+    );
+  }
+  async run(client: DiscordClient, message: Message, args: string[]) {
+    const user =
+      message.mentions.members.first() ||
+      message.guild.members.cache.find((u) => u.user.id == args[0]) ||
+      message.guild.members.cache.find((u) => u.user.username == args[0]) ||
+      message.guild.members.cache.find((u) => u.nickname == args[0]);
+    const argument = args[0];
 
-		if (user) {
-			const embed = await this.ImageEmbed.Base({
-				iconURL: user.user.displayAvatarURL({ dynamic: true }),
-				text: this,
-				title: `${user.user.tag}'s avatar`,
-				description: `Link: [Click here](${user.user.displayAvatarURL({
-					dynamic: true,
-				})})`,
-				image: user.user.displayAvatarURL({ dynamic: true }),
-			});
+    if (user) {
+      const embed = await this.ImageEmbed.Base({
+        iconURL: user.user.displayAvatarURL({ dynamic: true }),
+        text: this,
+        title: `${user.user.tag}'s avatar`,
+        description: `Link: [Click here](${user.user.displayAvatarURL({
+          dynamic: true,
+        })})`,
+        image: user.user.displayAvatarURL({ dynamic: true }),
+      });
 
-			return message.channel.send({ embeds: [embed] });
-		}
+      return message.channel.send({ embeds: [embed] });
+    }
 
-		if (argument) {
-			if (argument.toLowerCase().includes('help')) {
-				return this.HelpEmbed.Base({
-					iconURL: message.author.displayAvatarURL({ dynamic: true }),
-					command: this,
-					accessor: message,
-				});
-			} else {
-				try {
-					const profile = await client.users.fetch(argument);
+    if (argument) {
+      if (argument.toLowerCase().includes("help")) {
+        return this.HelpEmbed.Base({
+          iconURL: message.author.displayAvatarURL({ dynamic: true }),
+          command: this,
+          accessor: message,
+        });
+      } else {
+        try {
+          const profile = await client.users.fetch(argument);
 
-					const embed = await this.ImageEmbed.Base({
-						iconURL: profile.displayAvatarURL({ dynamic: true }),
-						text: this,
-						title: `${profile.tag}'s avatar`,
-						description: `Link: [Click here](${profile.displayAvatarURL({
-							dynamic: true,
-						})})`,
-						image: profile.displayAvatarURL({ dynamic: true }),
-					});
+          const embed = await this.ImageEmbed.Base({
+            iconURL: profile.displayAvatarURL({ dynamic: true }),
+            text: this,
+            title: `${profile.tag}'s avatar`,
+            description: `Link: [Click here](${profile.displayAvatarURL({
+              dynamic: true,
+            })})`,
+            image: profile.displayAvatarURL({ dynamic: true }),
+          });
 
-					return message.channel.send({ embeds: [embed] });
-				} catch (e) {
-					return;
-				}
-			}
-		}
+          return message.channel.send({ embeds: [embed] });
+        } catch (e) {
+          return;
+        }
+      }
+    }
 
-		if (!argument) {
-			const embed = await this.ImageEmbed.Base({
-				iconURL: message.author.displayAvatarURL({ dynamic: true }),
-				text: this,
-				title: `${message.author.tag}'s avatar`,
-				description: `Link: [Click here](${message.author.displayAvatarURL({
-					dynamic: true,
-				})})`,
-				image: message.author.displayAvatarURL({ dynamic: true }),
-			});
+    if (!argument) {
+      const embed = await this.ImageEmbed.Base({
+        iconURL: message.author.displayAvatarURL({ dynamic: true }),
+        text: this,
+        title: `${message.author.tag}'s avatar`,
+        description: `Link: [Click here](${message.author.displayAvatarURL({
+          dynamic: true,
+        })})`,
+        image: message.author.displayAvatarURL({ dynamic: true }),
+      });
 
-			return message.channel.send({ embeds: [embed] });
-		}
-	}
-	async slash(client: DiscordClient, interaction: CommandInteraction) {}
+      return message.channel.send({ embeds: [embed] });
+    }
+  }
+  async slash(client: DiscordClient, interaction: CommandInteraction) {}
 }
--- src/Commands/Server Utilities/Serverinfo.ts
+++ src/Commands/Server Utilities/Serverinfo.ts
@@ -1,107 +1,107 @@
-import BaseCommand from '../../Utils/Structures/BaseCommand';
-import DiscordClient from '../../Client/Client';
-import { CommandInteraction, Message, UserFlags } from 'discord.js';
+import BaseCommand from "../../Utils/Structures/BaseCommand";
+import DiscordClient from "../../Client/Client";
+import { CommandInteraction, Message, UserFlags } from "discord.js";
 
 export default class ServerinfoCommand extends BaseCommand {
-	constructor() {
-		super(
-			'serverinfo',
-			'server utilities',
-			[],
-			'',
-			"See the server's information",
-			'',
-			[],
-			[],
-			['SEND_MESSAGES', 'EMBED_LINKS'],
-			[],
-			true,
-			false,
-			false,
-			3000,
-			'debug'
-		);
-	}
-	async run(client: DiscordClient, message: Message, args: string[]) {
-		const serverinfo = {
-			name: `${message.guild.name}`,
-			id: `${message.guild.id}`,
-			ownerid: `${message.guild.ownerId}`,
-			available: `\`${message.guild.available ? 'Yes' : 'No'}\``,
-			large: `\`${message.guild.large ? 'Yes' : 'No'}\``,
-			partnered: `\`${message.guild.partnered ? 'Yes' : 'No'}\``,
-			verified: `\`${message.guild.verified ? 'Yes' : 'No'}\``,
-			membercount: `\`${message.guild.memberCount}\``,
-			humans: `\`${
-				message.guild.members.cache.filter((member) => !member.user.bot).size
-			}\``,
-			bots: `\`${
-				message.guild.members.cache.filter((member) => member.user.bot).size
-			}\``,
-			totalChannels: `\`${message.guild.channels.cache.size}\``,
-			textChannels: `\`${
-				message.guild.channels.cache.filter((ch) => ch.type === 'GUILD_TEXT')
-					.size
-			}\``,
-			voiceChannels: `\`${
-				message.guild.channels.cache.filter((ch) => ch.type === 'GUILD_VOICE')
-					.size
-			}\``,
-			contentFilter: `\`${message.guild.explicitContentFilter}\``,
-			verificationlvl: `\`${message.guild.verificationLevel}\``,
-			createdAt: `\`${message.guild.createdAt}\``,
-			boostCount: `\`${message.guild.premiumSubscriptionCount}\``,
-			boostTier: `\`${message.guild.premiumTier}\``,
-			roleCount: `\`${message.guild.roles.cache.size}\``,
-			roles: `\`${message.guild.roles.cache
-				.map((role) => role.toString())
-				.join(' ')}\``,
-		};
+  constructor() {
+    super(
+      "serverinfo",
+      "server utilities",
+      [],
+      "",
+      "See the server's information",
+      "",
+      [],
+      [],
+      ["SEND_MESSAGES", "EMBED_LINKS"],
+      [],
+      true,
+      false,
+      false,
+      3000,
+      "WIP"
+    );
+  }
+  async run(client: DiscordClient, message: Message, args: string[]) {
+    const serverinfo = {
+      name: `${message.guild.name}`,
+      id: `${message.guild.id}`,
+      ownerid: `${message.guild.ownerId}`,
+      available: `\`${message.guild.available ? "Yes" : "No"}\``,
+      large: `\`${message.guild.large ? "Yes" : "No"}\``,
+      partnered: `\`${message.guild.partnered ? "Yes" : "No"}\``,
+      verified: `\`${message.guild.verified ? "Yes" : "No"}\``,
+      membercount: `\`${message.guild.memberCount}\``,
+      humans: `\`${
+        message.guild.members.cache.filter((member) => !member.user.bot).size
+      }\``,
+      bots: `\`${
+        message.guild.members.cache.filter((member) => member.user.bot).size
+      }\``,
+      totalChannels: `\`${message.guild.channels.cache.size}\``,
+      textChannels: `\`${
+        message.guild.channels.cache.filter((ch) => ch.type === "GUILD_TEXT")
+          .size
+      }\``,
+      voiceChannels: `\`${
+        message.guild.channels.cache.filter((ch) => ch.type === "GUILD_VOICE")
+          .size
+      }\``,
+      contentFilter: `\`${message.guild.explicitContentFilter}\``,
+      verificationlvl: `\`${message.guild.verificationLevel}\``,
+      createdAt: `\`${message.guild.createdAt}\``,
+      boostCount: `\`${message.guild.premiumSubscriptionCount}\``,
+      boostTier: `\`${message.guild.premiumTier}\``,
+      roleCount: `\`${message.guild.roles.cache.size}\``,
+      roles: `\`${message.guild.roles.cache
+        .map((role) => role.toString())
+        .join(" ")}\``,
+    };
 
-		if (args[0]) {
-			if (args[0].toLowerCase().includes('help')) {
-				return await this.HelpEmbed.Base({
-					iconURL: message.author.displayAvatarURL({ dynamic: true }),
-					command: this,
-					accessor: message,
-				});
-			}
-		} else {
-			const serverinfoEmbed = await this.Embed.Base({
-				iconURL: message.author.displayAvatarURL({ dynamic: true }),
-				text: this,
-				title: `Serverinfo command`,
-				description: `${serverinfo.name} serverinfo`,
-				fields: [
-					{ name: 'ID', value: `${serverinfo.id}` },
-					{ name: 'Server owner ID:', value: `${serverinfo.ownerid}` },
-					{ name: 'Created at:', value: `${serverinfo.createdAt}` },
-					{
-						name: 'Total channels',
-						value: `${serverinfo.totalChannels}`,
-						inline: true,
-					},
-					{
-						name: 'Text channels',
-						value: `${serverinfo.textChannels}`,
-						inline: true,
-					},
-					{
-						name: 'Voice channels',
-						value: `${serverinfo.voiceChannels}`,
-						inline: true,
-					},
-					{
-						name: 'Membercount',
-						value: `${serverinfo.membercount}`,
-						inline: true,
-					},
-					{ name: 'Human count', value: `${serverinfo.humans}`, inline: true },
-					{ name: 'Bot count', value: `${serverinfo.bots}`, inline: true },
-				],
-			});
-			return message.channel.send({ embeds: [serverinfoEmbed] });
-		}
-	}
-	async slash(client: DiscordClient, interaction: CommandInteraction) {}
+    if (args[0]) {
+      if (args[0].toLowerCase().includes("help")) {
+        return await this.HelpEmbed.Base({
+          iconURL: message.author.displayAvatarURL({ dynamic: true }),
+          command: this,
+          accessor: message,
+        });
+      }
+    } else {
+      const serverinfoEmbed = await this.Embed.Base({
+        iconURL: message.author.displayAvatarURL({ dynamic: true }),
+        text: this,
+        title: `Serverinfo command`,
+        description: `${serverinfo.name} serverinfo`,
+        fields: [
+          { name: "ID", value: `${serverinfo.id}` },
+          { name: "Server owner ID:", value: `${serverinfo.ownerid}` },
+          { name: "Created at:", value: `${serverinfo.createdAt}` },
+          {
+            name: "Total channels",
+            value: `${serverinfo.totalChannels}`,
+            inline: true,
+          },
+          {
+            name: "Text channels",
+            value: `${serverinfo.textChannels}`,
+            inline: true,
+          },
+          {
+            name: "Voice channels",
+            value: `${serverinfo.voiceChannels}`,
+            inline: true,
+          },
+          {
+            name: "Membercount",
+            value: `${serverinfo.membercount}`,
+            inline: true,
+          },
+          { name: "Human count", value: `${serverinfo.humans}`, inline: true },
+          { name: "Bot count", value: `${serverinfo.bots}`, inline: true },
+        ],
+      });
+      return message.channel.send({ embeds: [serverinfoEmbed] });
+    }
+  }
+  async slash(client: DiscordClient, interaction: CommandInteraction) {}
 }
