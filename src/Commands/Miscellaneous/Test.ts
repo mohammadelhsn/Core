724898e8c4a8734b575748b73dd3d00f2ee55f2c
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-10-31 09:36:07 -0400

    Update Test.ts

--- src/Commands/Miscellaneous/Test.ts
+++ src/Commands/Miscellaneous/Test.ts
@@ -33,35 +33,7 @@ export default class TesstCommand extends BaseCommand {
 		);
 	}
 	async run(client: DiscordClient, message: Message, args: string[]) {
-		const embed = await this.Embed.Base({
-			description: 'Test',
-			accessor: message,
-			text: this,
-			fields: [
-				{
-					name: 'Check mark',
-					value: `${this.Emojis.upvote}, ${this.Emojis.downvote}, ${this.Emojis.verified}`,
-				},
-				{
-					name: 'Row 2 ',
-					value: `${this.Emojis.bruh} ${this.Emojis.eyesshaking} ${this.Emojis.cry} ${this.Emojis.wot}`,
-				},
-				{
-					name: 'Row 3',
-					value: `${this.Emojis.streaming_emoji} ${this.Emojis.online_emoji} ${this.Emojis.offline_emoji} ${this.Emojis.dnd_emoji} ${this.Emojis.idle_emoji}`,
-				},
-				{
-					name: 'Row 4',
-					value: `${this.Emojis.loading} ${this.Emojis.loading_discord} ${this.Emojis.bot_emoji} ${this.Emojis.information_emoji}`,
-				},
-				{
-					name: 'Row 5',
-					value: `${this.Emojis.hypesquad_brilliance} ${this.Emojis.hypesquad_bravery} ${this.Emojis.hypesquad_balance} ${this.Emojis.discord_partner} ${this.Emojis.discord_bot_dev}`,
-				},
-			],
-		});
-
-		return message.reply({ embeds: [embed] });
+		console.log(message.guild.emojis.cache.toJSON());
 	}
 	async slash(client: DiscordClient, interaction: CommandInteraction) {}
 }
