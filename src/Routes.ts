5f3f889baee8061ef055fd07c01f97d0023a7713
ProcessVersion <51869374+DeltaAbstract@users.noreply.github.com>
2021-11-30 19:35:06 -0500

    Update Routes.ts

--- src/Routes.ts
+++ src/Routes.ts
@@ -4,6 +4,10 @@ const router = Router();
 
 router.use((req, res, next) => next());
 
+router.use('docs', (req, res) => {
+	return res.json({ test: 'working!' });
+});
+
 router.use('/guilds/', (req, res) => {});
 
 export default Router;
